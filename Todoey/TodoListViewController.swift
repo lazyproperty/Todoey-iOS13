cdc0716a59d5a53ec4d2ef93c045759c8e2a8049
lazyproperty <lazyproperty@icloud.com>
2023-09-24 10:01:22 +0300

    Add TableView cell and change NavBar appearence

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Позвонить Маме", "Купить хлеб", "Заправить машину"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    // MARK: -  TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+   
 
 
 }
